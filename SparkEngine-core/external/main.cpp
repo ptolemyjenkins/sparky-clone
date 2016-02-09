e903c08673aea3dc50abc3a387954fff172fb959
ptolemy jenkins <ptolemyjenkins@blueyonder.co.uk>
2016-02-09 20:54:49 +0000

    can't include objects

--- SparkEngine-core/external/main.cpp
+++ SparkEngine-core/external/main.cpp
@@ -12,7 +12,7 @@ int main()
 	engine.start(&sparky::graphics::RenderingEngine());
 	return 0;*/
 	
-	fileLoader a = fileLoader("./res/models/toroid.obj");
+	fileLoader a = fileLoader("./res/models/COMTest.obj");
 	vec4 *vertices = new vec4[a.verts.size()];
 	for (int i = 0; i < a.verts.size(); i++)
 	{
@@ -23,7 +23,7 @@ int main()
 	for (int i = 0; i < a.norms.size(); i++)
 	{
 		norms[i] = a.norms[i];
-		std::cout << norms[i] << std::endl;
+		//std::cout << norms[i] << std::endl;
 	}
 
 	int* indices = new int[a.indices.size()];
@@ -38,7 +38,7 @@ int main()
 		nindices[i + 1] = a.nindices[i + 1];
 		nindices[i + 2] = a.nindices[i + 2];
 
-		std::cout << nindices[i] << nindices[i + 1] << nindices[i + 2] << std::endl;
+		//std::cout << nindices[i] << nindices[i + 1] << nindices[i + 2] << std::endl;
 	}
 	physics::A_solid mushroom = physics::A_solid(vertices, a.verts.size(), indices, a.indices.size(),
 		norms, a.norms.size(), nindices, a.nindices.size());
